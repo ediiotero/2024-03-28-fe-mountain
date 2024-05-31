d56a400e90c9ca16015c92d7346faab43d36f586
ediiotero <e.otero81@gmail.com>
2024-05-30 21:03:10 -0400

    week 9 ready

--- week-8/demo.js
+++ week-8/demo.js
@@ -2,6 +2,63 @@
 //https://www.youtube.com/watch?v=ZYa_NiOUTQo
 //https://www.youtube.com/watch?v=jM0WcyQWMSM
 
+class Person {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+    }
+
+    static sleep(number) {
+        console.log(`Slept ${number} of hours`)
+    }
+
+    welcome() {
+        console.log(`I'm ${this.name} and my age is ${this.age}`)
+    }
+
+    hello() {
+        console.log(`Hello my name is ${this.name}`)
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, gpa){
+        super(name, age)
+        this.gpa = gpa
+    }
+
+    hello() {
+        super.hello()
+        console.log(`My gpa is ${this.gpa}`)
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age, classSize) {
+        super(name, age)
+        this.classSize = classSize
+    }
+
+    hello() {
+        console.log(`My class size is ${this.classSize}`)
+    }
+}
 
 
-                                                                                                                                                                                                                         
+const person1 = new Person('Bruce', 33)   
+const person2 = new Person('Stephanie', 44)   
+// person1.welcome()
+// person1.hello()
+// person2.welcome()
+// person2.hello()
+
+const student1 = new Student('Toby', 3, '3.0')
+console.log(student1)
+student1.hello()
+student1.welcome()
+
+const teach1 = new Teacher('Peter', 50, 11)
+console.log(teach1)
+teach1.hello()
+
+Person.sleep(8)
--- week-8/index.js
+++ week-8/index.js
@@ -1,26 +1,32 @@
 // Using the object below
 // 1 console log the customer name
-// 2 console log using a template literal "shipping to 123 Main street Gotham NY 12345" 
+// 2 console log using a template literal "shipping to 123 Main street Gotham NY 12345"
 // 3 add a property of "orderNum" and set it to 111222, change age to 44, console.log the object
 
 const customer = {
-    name: "Toby",
-    age: 32,
-    address: {
-        street: "123 Main street",
-        city: "Gotham",
-        state: "NY",
-        zip: "12345"
-    }
-}
+  name: "Toby",
+  age: 32,
+  address: {
+    street: "123 Main street",
+    city: "Gotham",
+    state: "NY",
+    zip: "12345",
+  },
+};
 
-
+// console.log(customer.name);
+// console.log(
+//   `shipping to ${customer.address.street} ${customer.address.city} ${customer.address.state} ${customer.address.zip}`
+// );
+// customer.orderNum = 111222;
+// customer.age = 44;
+// console.log(customer);
 
 // ==================================
 
-// Write a class for a Hero 
+// Write a class for a Hero
 // - the Hero should have a property of name, health, and power
-// - the Hero should also have a method of attack 
+// - the Hero should also have a method of attack
 //   - attack should return power plus 5
 
 // Write another class that will inherit Hero and call it Villain
@@ -30,6 +36,46 @@ const customer = {
 
 // Create a hero and a villain and log out all of their properties
 
-// *Bonus - Write a loop that will have the hero(use attack) and villain(use magic) attack each other 
+// *Bonus - Write a loop that will have the hero(use attack) and villain(use magic) attack each other
 //          reducing the amount of health left and console.log each round until health is at 0 for one of them.
+
+class Hero {
+    constructor(name, health, power) {
+        this.name = name
+        this.health = health
+        this.power = power
+    }
+
+    attack() {
+        return this.power + 5
+    }
+}
+
+const hero1 = new Hero('Batman', 100, 5)
+console.log(hero1)
+console.log(hero1.attack())
+
+class Villain extends Hero {
+    constructor(name, health, power) {
+        super(name, health, power)
+    }
+
+    magic() {
+        return this.power * 2
+    }
+}
+
+const villain1 = new Villain('Black Adam', 125, 8)
+console.log(villain1)
+console.log(villain1.magic())
+
+while(hero1.health > 0 && villain1.health > 0) {
+    const villainMagic = villain1.magic()
+    const heroAttack = hero1.attack()
+
+    hero1.health = hero1.health - villainMagic
+    villain1.health = villain1.health - heroAttack
+
+    console.log(`Current health of ${hero1.name} is ${hero1.health} and ${villain1.name} health is ${villain1.health}`)
+}
 
