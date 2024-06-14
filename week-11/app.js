861f5f5c5459cb29e730a8e5f82d20fe530600dc
ediiotero <e.otero81@gmail.com>
2024-06-13 22:23:29 -0400

    week 11 done

--- week-11/app.js
+++ week-11/app.js
@@ -6,4 +6,19 @@
 
 $(document).ready(() => {
     //your code here
+    $('#myForm').submit((event) => {
+        event.preventDefault()
+        const formData = {
+            title: $('#title').val(),
+            body: $('#body').val()
+        }
+
+        $.post(
+            'https://jsonplaceholder.typicode.com/posts',
+            formData,
+            (data) => { console.log(data) }
+        )
+
+        $('#myForm').trigger('reset')
+    })
 })
